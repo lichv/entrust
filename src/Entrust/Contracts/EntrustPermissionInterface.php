b90f8c48e491ec4d5d60f2f75a56e34ce5250acf
lichv <lichvg@126.com>
2018-12-18 11:47:51 +0800

    fork https://github.com/Zizaco/entrust

---
+++ src/Entrust/Contracts/EntrustPermissionInterface.php
@@ -0,0 +1,20 @@
+<?php namespace Lichv\Entrust\Contracts;
+
+/**
+ * This file is part of Entrust,
+ * a role & permission management solution for Laravel.
+ *
+ * @license MIT
+ * @package Lichv\Entrust
+ */
+
+interface EntrustPermissionInterface
+{
+
+    /**
+     * Many-to-Many relations with role model.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function roles();
+}
